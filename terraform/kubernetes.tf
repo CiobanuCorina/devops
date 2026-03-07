3ffe9b0e91b7d247ad2c17931565d247fddabb3b
Corina Ciobanu <corina.ciobanu@dsi.md>
2026-03-07 21:09:39 +0200

    configure terraform deploy

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -2,11 +2,3 @@ provider "kubernetes" {
   host = var.openshift_api_url
   token = var.openshift_token
 }
-
-data "kubernetes_service_v1" "devops_service" {
-  depends_on = [helm_release.devops_helm]
-  metadata {
-    name = "devops-service"
-  }
-  namespace  = var.namespace
-}
